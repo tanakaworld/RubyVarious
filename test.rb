dd45755715fd9fe93998d0bab8b6d85a23463849
yutaro <y.tanaka.kcbbf@gmail.com>
2014-01-29 00:16:51 +0900

    Output Double Value w/ abreviated notation.

--- test.rb
+++ test.rb
@@ -1,2 +1,10 @@
-input_value = gets
-puts input_value
+#input_value = gets
+#puts input_value
+
+# Return Double Input Value.
+#input = gets
+#num = input.to_i
+#puts num * 2
+
+
+puts gets.to_i * 5
